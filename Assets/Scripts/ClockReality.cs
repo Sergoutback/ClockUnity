06b1d3d603f08b450ab76394388c21df41363758
SergoutbackDesktop <webart2013@gmail.com>
2022-01-20 23:57:13 +0300

    Initial commit

---
+++ Assets/Scripts/ClockReality.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ClockReality : MonoBehaviour
+{
+    float secondsTextNew = 60f, minutesTextNew = 60f, hoursTextNew = 12f;
+
+    public Text hoursText, minutesText, secondText;
+
+
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+                
+        hoursText.text = "" + (time.Hour * (12f / hoursTextNew)) + "  :";
+        minutesText.text = "" + (time.Minute * (60f / minutesTextNew)) + "  :";
+        secondText.text = "" + (time.Second * (60f / secondsTextNew));
+    }
+}
